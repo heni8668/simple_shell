39bd513706187da006a1edda811d9aed2f87bbcf
heni8668 <70965503+heni8668@users.noreply.github.com>
2022-05-18 15:00:26 +0300

    Add files via upload
    commit

---
+++ simple_shell.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+echo "/bin/ls" | ./hsh
+
+echo "ls -l /tmp" | ./hsh
+
+echo "env" | ./hsh
+
+echo "ls /var ; ls /var" |./hsh
+
+echo "ls /hbtn ; ls /var" | ./hsh
+
+echo "ls /var && ls /var && ls /var && ls /hbtn" | ./hsh
+
+echo "ls /var && ls /var && ls /var && ls /hbtn" | ./hsh
+
+echo "echo $?" |./hsh
+
+echo "echo $$" |./hsh
+
+echo "echo $PATH" |./hsh
+
+echo "echo $$ # ls -la" | ./hsh
